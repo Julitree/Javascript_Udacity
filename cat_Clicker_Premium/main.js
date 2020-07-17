3e3bed56cad579cf6cb7a11f57b6c00bd343f9df
julytree@hotmail.es <Dulcecafe1981>
2020-07-17 09:29:22 +0200

    Udacity_JS

--- cat_Clicker_Premium/main.js
+++ cat_Clicker_Premium/main.js
@@ -1,12 +1,22 @@
 const nameWrap = document.getElementById('nameWrap');   
-const imageWrap = document.getElementById('imageWrap')
+const imageWrap = document.getElementById('imageWrap');
+const buttonClicker = document.getElementById('buttonClicker');
+
+
+let image = document.createElement("img");
+let p = document.createElement('p');
+imageWrap.appendChild(p);
+imageWrap.appendChild(image);
+image.setAttribute("width", "304");
+image.setAttribute("height", "228");
+
 
 const main = {
     "Philippe": "cat.jpg",
     "Sondra": "catTwo.jpg",
     "Antoinne": "3.jpg",
     "Lulu": "4.jpg",
-    "coco": "5.jpg"
+    "Coco": "5.jpg"
 }
 
 
@@ -16,24 +26,20 @@ nameWrap.appendChild(ulItem);
 for (key in main) {
     let liItem = document.createElement('li');
     liItem.innerText = key
-    ulItem.appendChild(liItem)
-    
-    liItem.addEventListener('click', function(event) {
-        let item = event.target;
-        let objectKey =  item.innerText
-        let foto = main[objectKey];
-        console.log('"' + objectKey + '":' + '"' + main[objectKey] + '"')
-        
-     
-        
-        
-        var image = document.createElement("IMG");
-        image.setAttribute("src", foto);
-        imageWrap.appendChild(image)
-        image.setAttribute("width", "304");
-        image .setAttribute("height", "228");
+    ulItem.appendChild(liItem) 
 
+     
+    liItem.addEventListener('click', function(event) {
         
-    })
+        let item = event.target;
+        p.innerHTML = `I introduce to you my cat ${item.textContent}`
+        let objectKey =  item.innerText;        
+        let src = main[objectKey];
+        image.setAttribute("src", src);        
+      
+    })  
+       
+    buttonClicker.style.display = 'block';
+
 }
 
